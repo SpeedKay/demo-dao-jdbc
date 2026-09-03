6a47667fd0e8fb5dbe537ed83df3b431a904b7f8
Kayke <kayke@email.com>
2026-09-03 14:36:54 -0300

    Classe Departament

--- src/application/progarm.java
+++ src/application/progarm.java
@@ -3,13 +3,14 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entities.Departament;
 
 public class progarm {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Departament obj = new Departament(1, "Books");
+		System.out.println(obj);
 
 	}
 
