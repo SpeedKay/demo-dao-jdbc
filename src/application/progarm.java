2e6d9036609d59c9ad786c497c3653922604a35f
Kayke <kayke@email.com>
2026-09-04 14:27:49 -0300

    Classe Seller

--- src/application/progarm.java
+++ src/application/progarm.java
@@ -1,17 +1,20 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class progarm {
 
 	public static void main(String[] args) {
 		
 		Departament obj = new Departament(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
 		System.out.println(obj);
-		System.out.println("tomara");
+		System.out.println(seller);
 
 	}
 
