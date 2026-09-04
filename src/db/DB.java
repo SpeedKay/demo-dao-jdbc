817e63a28b13b18b2ac1c9c26a406bc213acdbe6
Kayke <kayke@email.com>
2026-09-04 14:53:40 -0300

    Interfaces Dao

--- src/db/DB.java
+++ src/db/DB.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
-
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -13,13 +13,13 @@ public class Seller implements Serializable{
 	private Date birthDate;
 	private Double baseSalary;
 	
-	private Departament departament;
+	private Department departament;
 	
 	public Seller() {
 	}
 
 	public Seller(Integer id, String name, String emailString, Date birthDate, Double baseSalary,
-			Departament departament) {
+			Department departament) {
 		this.id = id;
 		this.name = name;
 		this.emailString = emailString;
@@ -68,11 +68,11 @@ public class Seller implements Serializable{
 		this.baseSalary = baseSalary;
 	}
 
-	public Departament getDepartament() {
+	public Department getDepartament() {
 		return departament;
 	}
 
-	public void setDepartament(Departament departament) {
+	public void setDepartament(Department departament) {
 		this.departament = departament;
 	}
 
